12f5e4eb42e1f5c0c840535fdf0d3a89e20b5196
Coopex Entregas <thalesalcantara88@gmail.com>
2025-07-01 09:12:55 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// firebase.js
 const firebaseConfig = {
   apiKey: "AIzaSyApakPCq5s-qySSOroY9a4X85fFC1iTfvA",
   authDomain: "escslas.firebaseapp.com",
@@ -8,5 +7,4 @@ const firebaseConfig = {
   appId: "1:804887685890:web:af0df71f8dd97ca35df01b",
   measurementId: "G-SBKXC7MTJX"
 };
-
 firebase.initializeApp(firebaseConfig);
